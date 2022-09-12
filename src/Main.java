7b0bb52e19a01b8090c99b5f6d0d00aec1ec385e
Prosikova <lvenok182@mail.ru>
2022-09-12 15:10:23 +0300

    Выполнено задание 6 в домашней работе 2_3

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+var boxerWeightOne = 78.2;
+var boxerWeightTwo = 82.7;
+var boxersWeight = boxerWeightOne + boxerWeightTwo;
+        System.out.println("Общий вес боксеров " + boxersWeight + " кг!");
+        var weightDifference = boxerWeightOne - boxerWeightTwo;
+        System.out.println("Разница между весами " + weightDifference + " кг!");
+    }
+}
